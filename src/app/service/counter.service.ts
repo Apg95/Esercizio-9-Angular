39bb928af6361581b472fcf6ee931167cf560708
Andrea Paola Girgenti <andreapaolagirgenti@gmail.com>
2021-12-07 16:48:53 +0100

    initial

--- src/app/service/counter.service.ts
+++ src/app/service/counter.service.ts
@@ -6,27 +6,9 @@ import { ControlContainer } from '@angular/forms';
 })
 export class CounterService {
 
-  private counter = 0;
+  
 
   constructor() { }
 
-  recoverCounter(): number {
-    return this.counter
-  }
-
-  counterIncrease(num: any) {
-    if (this.counter + num >= 0) {
-      return this.counter += num;
-    } else {
-      return console.log('Errore')
-    }
-  }
   
-  counterDecrease(num: any)  {
-    if (this.counter - num >= 0) {
-      return this.counter -= num;
-    } else {
-      return console.log('Errore');
-    }
-}
 }
